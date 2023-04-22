131631532172fbb4f4c995eecc98cec4fccedc14
Ascend Ho <coderhs@foxmail.com>
2023-04-22 10:25:14 +0800

    💡nullable types

--- src/03 - Advanced Types/dist/index.js
+++ src/03 - Advanced Types/dist/index.js
@@ -1,3 +1,28 @@
 "use strict";
-console.log('advanced type');
+let employee = {
+    id: 1,
+    name: 'Mosh',
+    retire: (date) => {
+        console.log(date);
+    }
+};
+function kgToLbs(weight) {
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    return parseInt(weight) * 2.2;
+}
+let textBox = {
+    drag: () => {
+    },
+    resize: () => {
+    }
+};
+let quantity = 100;
+function greet(name) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log('Hola');
+}
+greet(undefined);
 //# sourceMappingURL=index.js.map
--- src/03 - Advanced Types/src/index.ts
+++ src/03 - Advanced Types/src/index.ts
@@ -1,1 +1,75 @@
-console.log('advanced type');
+// Type Aliases
+
+type Employee = {
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+}
+
+let employee: Employee = {
+    id: 1,
+    name: 'Mosh',
+    retire: (date: Date) => {
+        console.log(date);
+    }
+}
+
+// Union Types
+
+function kgToLbs(weight: number | string): number {
+    // Narrowing
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    return parseInt(weight) * 2.2;
+
+}
+
+// Intersection Types
+
+type Draggable = {
+    drag: () => void
+}
+
+type Resizable = {
+    resize: () => void
+}
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+    drag: () => {
+    },
+    resize: () => {
+    }
+}
+
+// Literal Types
+
+type Quantity = 50 | 100;
+let quantity: Quantity = 100;
+
+type Metric = 'cm' | 'inch';
+
+// Nullable Types
+
+function greet(name: string | null | undefined) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log('Hola');
+}
+
+greet(undefined);
+
+
+
+
+
+
+
+
+
+
+
+
+
