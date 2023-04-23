eb538cc5a6c66bab94774fd618f6c1164be8694f
Ascend Ho <coderhs@foxmail.com>
2023-04-23 20:57:18 +0800

    💡 Creating Objects

---
+++ src/04 - Classes, Interfaces and Object-oriented Programming/dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+class Account {
+    constructor(id, owner, balance) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+    deposit(amount) {
+        if (amount <= 0)
+            throw new Error('Invalid amount');
+        this.balance += amount;
+    }
+}
+let account = new Account(1, 'Mosh', 0);
+account.deposit(100);
+console.log(typeof account);
+console.log(account instanceof Account);
+//# sourceMappingURL=index.js.map
--- src/04 - Classes, Interfaces and Object-oriented Programming/src/index.ts
+++ src/04 - Classes, Interfaces and Object-oriented Programming/src/index.ts
@@ -1,5 +1,32 @@
 /* Creating Classes */
 
+class Account {
+    id: number;
+    owner: string;
+    balance: number;
+
+    constructor(id: number, owner: string, balance: number) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    deposit(amount: number): void {
+        if (amount <= 0)
+            throw new Error('Invalid amount');
+        this.balance += amount;
+    }
+
+}
+
+/* Creating Objects */
+
+let account = new Account(1, 'Mosh', 0);
+account.deposit(100);
+console.log(typeof account);               //object
+console.log(account instanceof Account);   //true
+
+
 
 
 
