f0122aca9ccbce4a343c6e5a3de289fcfec34c5a
Ascend Ho <coderhs@foxmail.com>
2023-04-24 21:45:51 +0800

    💡Static Members

--- src/04 - Classes, Interfaces and Object-oriented Programming/dist/index.js
+++ src/04 - Classes, Interfaces and Object-oriented Programming/dist/index.js
@@ -1,18 +1,46 @@
 "use strict";
 class Account {
-    constructor(id, owner, balance) {
+    constructor(id, owner, _balance) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = _balance;
     }
     deposit(amount) {
         if (amount <= 0)
             throw new Error('Invalid amount');
-        this.balance += amount;
+        this._balance += amount;
+    }
+    get balance() {
+        return this._balance;
     }
 }
 let account = new Account(1, 'Mosh', 0);
 account.deposit(100);
 console.log(typeof account);
 console.log(account instanceof Account);
+console.log(account.balance);
+class SeatAssignments {
+}
+let seats = new SeatAssignments();
+seats.A1 = 'Mosh';
+seats.A2 = 'John';
+console.log(seats);
+class Ride {
+    start() {
+        Ride._activeRides++;
+    }
+    stop() {
+        Ride._activeRides--;
+    }
+    ;
+    static get activeRides() {
+        return this._activeRides;
+    }
+}
+Ride._activeRides = 0;
+let ride1 = new Ride();
+ride1.start();
+let ride2 = new Ride();
+ride2.start();
+console.log(Ride.activeRides);
 //# sourceMappingURL=index.js.map
--- src/04 - Classes, Interfaces and Object-oriented Programming/src/index.ts
+++ src/04 - Classes, Interfaces and Object-oriented Programming/src/index.ts
@@ -1,36 +1,85 @@
 /* Creating Classes */
 
 class Account {
-    id: number;
-    owner: string;
-    balance: number;
+    // make nickname optional
+    nickname?: string;
 
-    constructor(id: number, owner: string, balance: number) {
-        this.id = id;
-        this.owner = owner;
-        this.balance = balance;
+    constructor(
+        public readonly id: number,
+        public owner: string,
+        private _balance: number) {
     }
 
     deposit(amount: number): void {
         if (amount <= 0)
             throw new Error('Invalid amount');
-        this.balance += amount;
+        this._balance += amount;
     }
 
+
+    get balance(): number {
+        return this._balance;
+    }
+
+
+    // set balance(value: number) {
+    //     if (value < 0)
+    //         throw new Error('Invalid value');
+    //
+    //     this._balance = value;
+    // }
 }
 
 /* Creating Objects */
 
 let account = new Account(1, 'Mosh', 0);
 account.deposit(100);
+
 console.log(typeof account);               //object
 console.log(account instanceof Account);   //true
+console.log(account.balance);
 
 
+/* Index Signatures */
+
+class SeatAssignments {
+    [seatNumber: string]: string
+
+}
+
+let seats = new SeatAssignments();
+
+seats.A1 = 'Mosh';
+seats.A2 = 'John';
+
+// identical
+// seats['A1'] = 'Mosh';
+console.log(seats);
+
+/* Static Members */
+
+class Ride {
+    private static _activeRides: number = 0;
+
+    start() {
+        Ride._activeRides++;
+    }
+
+    stop() {
+        Ride._activeRides--
+    };
 
 
+    static get activeRides(): number {
+        return this._activeRides;
+    }
+}
 
+let ride1 = new Ride();
+ride1.start();
 
+let ride2 = new Ride();
+ride2.start();
 
-
+console.log(Ride.activeRides);
 
