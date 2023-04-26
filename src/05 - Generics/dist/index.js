294d0d30138e1dc6a9cc30756d425350d71d4aa4
Ascend Ho <coderhs@foxmail.com>
2023-04-26 11:10:33 +0800

    💡 Generic Constraints

--- src/05 - Generics/dist/index.js
+++ src/05 - Generics/dist/index.js
@@ -1,3 +1,47 @@
 "use strict";
-console.log('hello world');
+var _a;
+
+class KeyValuePair {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+let pair = new KeyValuePair('1', 'a');
+console.log(pair);
+
+class ArrayUtils {
+    static wrapInArray(value) {
+        return [value];
+    }
+}
+
+let numbers = ArrayUtils.wrapInArray('1');
+console.log(numbers);
+
+function fetch(url) {
+    if (url)
+        return {data: null, error: null};
+    else
+        return {data: null, error: "url is invalid"};
+}
+
+let result = fetch('url');
+console.log((_a = result.data) === null || _a === void 0 ? void 0 : _a.title);
+
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+class Customer extends Person {
+}
+
+function echo(value) {
+    return value;
+}
+
+console.log(echo(new Customer('A')));
 //# sourceMappingURL=index.js.map
--- src/05 - Generics/src/index.ts
+++ src/05 - Generics/src/index.ts
@@ -1,1 +1,68 @@
-console.log('hello world')
+/* Understanding the Problem */
+
+/* Generic Classes */
+
+class KeyValuePair<K, V> {
+    constructor(public key: K, public value: V) {
+    }
+}
+
+let pair = new KeyValuePair('1', 'a');
+
+console.log(pair);
+
+/* Generic Functions */
+
+class ArrayUtils {
+    static wrapInArray<T>(value: T) {
+        return [value];
+    }
+}
+
+let numbers = ArrayUtils.wrapInArray('1');
+
+console.log(numbers);
+
+/* Generic Interfaces */
+
+interface Result<T> {
+    data: T | null,
+    error: string | null
+
+}
+
+function fetch<T>(url: string): Result<T> {
+    if (url)
+        return {data: null, error: null};
+    else
+        return {data: null, error: "url is invalid"};
+}
+
+interface User {
+    username: string;
+}
+
+interface Product {
+    title: string;
+}
+
+let result = fetch<Product>('url');
+
+console.log(result.data?.title);
+
+/* Generic Constraints */
+
+class Person {
+    constructor(public name: string) {
+    }
+}
+
+class Customer extends Person {
+
+}
+
+function echo<T extends Person>(value: T): T {
+    return value;
+}
+
+console.log(echo(new Customer('A')));
