765714cbfa0074af607276006555e390b5ca3a23
Ascend Ho <coderhs@foxmail.com>
2023-04-27 10:32:54 +0800

    💡Extending Generic Classes

--- src/05 - Generics/dist/index.js
+++ src/05 - Generics/dist/index.js
@@ -1,47 +1,59 @@
 "use strict";
 var _a;
-
 class KeyValuePair {
     constructor(key, value) {
         this.key = key;
         this.value = value;
     }
 }
-
 let pair = new KeyValuePair('1', 'a');
 console.log(pair);
-
 class ArrayUtils {
     static wrapInArray(value) {
         return [value];
     }
 }
-
 let numbers = ArrayUtils.wrapInArray('1');
 console.log(numbers);
-
 function fetch(url) {
     if (url)
-        return {data: null, error: null};
+        return { data: null, error: null };
     else
-        return {data: null, error: "url is invalid"};
+        return { data: null, error: "url is invalid" };
 }
-
 let result = fetch('url');
 console.log((_a = result.data) === null || _a === void 0 ? void 0 : _a.title);
-
 class Person {
     constructor(name) {
         this.name = name;
     }
 }
-
 class Customer extends Person {
 }
-
 function echo(value) {
     return value;
 }
-
 console.log(echo(new Customer('A')));
+class Store {
+    constructor() {
+        this._objects = [];
+    }
+    add(obj) {
+        this._objects.push(obj);
+    }
+}
+class CompressibleStore extends Store {
+    compress() {
+    }
+}
+class SearchableStore extends Store {
+    find(name) {
+        return this._objects.find(obj => obj.name === name);
+    }
+}
+class ProductStore extends Store {
+    filterByCategory(category) {
+        return [];
+    }
+}
 //# sourceMappingURL=index.js.map
--- src/05 - Generics/src/index.ts
+++ src/05 - Generics/src/index.ts
@@ -66,3 +66,48 @@ function echo<T extends Person>(value: T): T {
 }
 
 console.log(echo(new Customer('A')));
+
+/* Extending Generic Classes */
+
+interface Product {
+    name: string,
+    price: number;
+}
+
+class Store<T> {
+    protected _objects: T[] = [];
+
+    add(obj: T): void {
+        this._objects.push(obj);
+    }
+}
+
+// Pass on the generic type parameter
+
+class CompressibleStore<T> extends Store<T> {
+    compress() {
+    }
+}
+
+// Restrict the generic type parameter
+
+class SearchableStore<T extends { name: string }> extends Store<T> {
+    find(name: string): T | undefined {
+        return this._objects.find(obj => obj.name === name);
+    }
+}
+
+// Fix the generic type parameter
+
+class ProductStore extends Store<Product> {
+    filterByCategory(category: string): Product[] {
+        return [];
+    }
+}
+
+
+
+
+
+
+
